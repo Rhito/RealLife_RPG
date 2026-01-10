73eaff2552847a8c74235f3d76785ffe7b3e81ba
Rhito <lasnguyen06@gmail.com>
2026-01-10 13:41:15 +0700

    feat: Update password reset functionality to use deep links and remove old views

--- RealLife_RPG/routes/web.php
+++ RealLife_RPG/routes/web.php
@@ -11,6 +11,4 @@ Route::get('/logError', function () {
 });
 
 // Password reset web redirect (for clicking email links in browser)
-// Password reset web interface
-Route::get('/password-reset-web', [\App\Http\Controllers\Auth\PasswordResetWebController::class, 'showResetForm'])->name('password.reset.web');
-Route::post('/password-reset-web', [\App\Http\Controllers\Auth\PasswordResetWebController::class, 'reset'])->name('password.update.web');
+Route::get('/password-reset-web', [\App\Http\Controllers\Auth\PasswordResetWebController::class, 'showResetRedirect']);
