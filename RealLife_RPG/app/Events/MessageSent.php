72c4e32839b76768d4d832c4a0801bd0569d7235
Rhito <lasnguyen06@gmail.com>
2026-01-28 21:19:37 +0700

    feat: update function queue message

--- RealLife_RPG/app/Events/MessageSent.php
+++ RealLife_RPG/app/Events/MessageSent.php
@@ -9,7 +9,6 @@ use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use App\Models\Message;
 
 class MessageSent implements ShouldBroadcast
